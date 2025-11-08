996da01c9afda9b516106406121bf367714933d9
dlng23 <1myungjin@ewhain.net>
2025-11-08 16:39:02 +0900

    Refactor deterministic_checker to use UDSMessage

--- main.py
+++ main.py
@@ -77,10 +77,10 @@ def fail(data, udsid, sid, depth, dq, msg_idx):
 
 def deterministic_checker(data, bus, udsid, sid, depth, dq):
     global msg_idx
-    mutated_data_list = deterministic_mutator(data)
+    msg = UDSMessage(udsid, sid, mutated_data, depth, bus)
+    mutated_data_list = deterministic_mutator(msg)
     fail_checker = False
     for mutated_data in mutated_data_list:
-        msg = UDSMessage(udsid, sid, mutated_data, depth, bus)
         save_log(msg_idx, udsid, sid, mutated_data)
         msg_idx += 1
 
@@ -103,7 +103,7 @@ def test_deque(dq, bus):
     else:
         if depth < MAX_DEPTH:
             if not deterministic_checker(data, bus, udsid, sid, depth, dq):
-                for mutated_data in nondeterministic_mutator(data):
+                for mutated_data in nondeterministic_mutator(msg):
                     dq.append((udsid, sid, mutated_data, depth+1))
 
 def main():
