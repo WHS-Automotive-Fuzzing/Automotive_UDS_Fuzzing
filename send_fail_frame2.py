d866872332b8cac620db55605d19bbf07ffb6ca8
SJ <s3037444@kookmin.ac.kr>
2026-01-03 15:47:48 +0900

    Add send_fail_frame

--- send_fail_frame2.py
+++ send_fail_frame2.py
@@ -5,8 +5,19 @@ import time
 import signal
 import sys
 from collections import deque
-from module.uds_sender import UDSSender
+from module.uds_send import *
 
+
+
+Response_ID = {
+    0x73E: 0x7A8,
+    0x70E: 0x778,
+    0x74C: 0x7B6,
+    0x723: 0x78D,
+    0x74B: 0x7B5,
+    0x74A: 0x7B4,
+    0x17FC0084: 0x17FE0084,
+}
 # CSV 파일 읽기
 records = []
 with open('test.csv', newline='') as f:
@@ -23,46 +34,24 @@ with open('test.csv', newline='') as f:
 
 # CAN 버스 및 ISO-TP 스택 설정
 bus = can.interface.Bus(channel='can0', bustype='socketcan')
-
-# ISO-TP 스택 초기화 (첫 번째 메시지의 UDS ID 사용)
-if records:
-    first_udsid = records[0][1]
-    isotp_params = isotp.params.TransportLayerParameters(
-        stmin=0,
-        blocksize=0,
-        tx_padding=0x00,
-        rx_flowcontrol_timeout=1000,
-        rx_consecutive_frame_timeout=1000
-    )
     
-    addr = isotp.Address(
-        isotp.AddressingMode.Normal_11bits,
-        txid=first_udsid,
-        rxid=first_udsid + 8
-    )
-    
-    stack = isotp.CanStack(bus=bus, address=addr, params=isotp_params)
-    uds_sender = UDSSender(stack)
 
 # 메시지 하나씩 전송
 while records:
     msg_idx, udsid, sid, data = records.pop(0)
     
-    # ISO-TP 주소 업데이트 (UDS ID가 변경되는 경우)
-    if addr.txid != udsid:
-        addr = isotp.Address(
-            isotp.AddressingMode.Normal_11bits,
-            txid=udsid,
-            rxid=udsid + 8
-        )
-        stack = isotp.CanStack(bus=bus, address=addr, params=isotp_params)
-        uds_sender = UDSSender(stack)
+    addr = isotp.Address(isotp.AddressingMode.Normal_11bits, txid=udsid, rxid=Response_ID[udsid])
+    params = {"tx_padding": 0xFF}
+    stack = isotp.CanStack(bus= bus, address=addr, params=params)
+    sender = UDSSender(stack)
+    
+   
     
     # 진단 모드 진입 + 메시지 전송
     message = [sid] + data
     print(f"\n[MSG {msg_idx:04X}] Sending to 0x{udsid:03X}: {bytes(message).hex()}")
     
-    success, response, diag_resp = uds_sender.StartDiagnosticAndSendMessage(
+    success, response, diag_resp = sender.StartDiagnosticAndSendMessage(
         message=message,
         session_type=0x03,
         retry_count=3
