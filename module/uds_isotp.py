88f6fee0c170e0f6d17fd37e12ac522e2a2dacc7
cocaoreo <124383281+cocaoreo@users.noreply.github.com>
2026-04-06 21:51:04 +0900

    Enhance Check_ECU_Alive with Testerpreset
    Added retry logic to Check_ECU_Alive method for improved reliability.

--- module/uds_isotp.py
+++ module/uds_isotp.py
@@ -79,7 +79,10 @@ class UDSMessage:
         
         return Diagnosticmode_fail
 
-    def Check_ECU_Alive(self, sender):        
+    def Check_ECU_Alive(self, sender):
+        success, response = sender.SendTesterPresent(retry_count=3)
+        if not success:
+            print("ECU_ALIVE_TesterPresent Fail")
         success, response= sender.EnterDiagnosticSession(session_type=0x01, retry_count=5)
         if not success:
             print(f"[{hex(self.udsid)}][{hex(self.sid)}][{response}] Fail Detected: no response 10 01")
